7d1d0c53b960d7dcc7c463ac5dd230dbb95dd503
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-10-19 21:35:14 -0700

    fail first test again due to using findByText method & having multiple elements w/same text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,13 +8,13 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Ionic React Todos</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Ionic React Todos</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
