b4755f6cef741ae639ad80ecce49e3c7efcb25ff
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-10-19 21:39:46 -0700

    write second test re: no todos message and fail

--- src/pages/Home.test.tsx
+++ src/pages/Home.test.tsx
@@ -5,7 +5,12 @@ import Home from './Home';
 test('page should have a title of Ionic React Todos', async () => {
   const { findByText } = render(<Home />);
   await findByText('Ionic React Todos');
-})
+});
 
 //render method takes our component and returns a bunch of helper methods to aid us in selecting pieces of teh DOM that was rendered. 
 //The findByText method is one of them. It looks for a n element that contains the text passed into it. If it doesn't find one of if it finds more thann one, it throws an error.
+
+test('page should render no todos message when there are no todos', async () => {
+  const { findByText } = render(<Home />);
+  await findByText('No todos, add some!')
+});
